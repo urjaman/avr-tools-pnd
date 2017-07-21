238633f84f8fdcc31161db8d755b7e60f0eaf211
Urja Rannikko <urjaman@gmail.com>
2017-07-22 02:39:57 +0300

    Add LD_LIBRARY_PATH to init.sh (for AVaRICE, libstdc++)
    sorry guys, you'll need to integrate this yourself or wipe init.sh from
    appdata if you've changed init.sh, but thats life

--- pnd/init.sh
+++ pnd/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 . /etc/profile
 export PATH=/mnt/utmp/avrtools/bin:$PATH
+export LD_LIBRARY_PATH=/mnt/utmp/avrtools/lib
 cd /mnt/utmp/avrtools
 echo "These are your extra tools in this shell:"
 ls /mnt/utmp/avrtools/bin
