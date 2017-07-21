5aae6d6eadbd0a6b8d9dbadc4b89ec22fedbc23d
Urja Rannikko <urjaman@gmail.com>
2017-07-22 02:16:58 +0300

    new make-pnd mods for avarice needing libstdc++

--- make-pnd.sh
+++ make-pnd.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Final touches on the PND (including the PND files), and make the pnd...
-cp -va pnd/* /mnt/utmp/avrtools/
-cp versions.txt /mnt/utmp/avrtools/
-chmod -R go+u /mnt/utmp/avrtools/
+PREFIX=/mnt/utmp/avrtools
+cp -va pnd/* $PREFIX/
+cp versions.txt $PREFIX/
+cp -vL /usr/gcc6/lib/libstdc++.so.6 $PREFIX/lib
+strip $PREFIX/lib/libstdc++.so.6
+chmod -R go+u $PREFIX/
+rm -f avrtools.sqfs
 mksquashfs /mnt/utmp/avrtools avrtools.sqfs -all-root
 cat avrtools.sqfs pnd/PXML.xml pnd/icon.png > avrtools.pnd
