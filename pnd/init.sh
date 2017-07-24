4eef5a928bf77b415f8f20dbd0a62b5b98e688e0
Urja Rannikko <urjaman@gmail.com>
2017-07-24 13:00:12 +0300

    something unsets LD_LIBRARY_PATH, so ... just re-set it in init.sh
    Meh.

--- pnd/init.sh
+++ pnd/init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export LD_LIBRARY_PATH=/mnt/utmp/avrtools/lib
 [ -e README ] && echo "Hint: Use 'less README' for more info."
 echo "These are your extra tools in this shell:"
 ls /mnt/utmp/avrtools/bin
