1867cc9fd3583f358ee66617a73460cfb3d175ce
Urja Rannikko <urjaman@gmail.com>
2017-07-24 12:06:27 +0300

    rewamp the PND scripts, so maybe opening tabs in the Terminal would be sane

--- pnd/init.sh
+++ pnd/init.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-. /etc/profile
-export PATH=/mnt/utmp/avrtools/bin:$PATH
-export LD_LIBRARY_PATH=/mnt/utmp/avrtools/lib
-cd /mnt/utmp/avrtools/src
 echo "These are your extra tools in this shell:"
 ls /mnt/utmp/avrtools/bin
 echo "Happy Hacking!"
--- pnd/run.sh
+++ pnd/run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-Terminal --disable-server -T "AVR Tools"  --maximize -e ./init.sh
+. /etc/profile
+export LANG=en_US.UTF-8
+export PATH=/mnt/utmp/avrtools/bin:$PATH
+export LD_LIBRARY_PATH=/mnt/utmp/avrtools/lib
+cd /mnt/utmp/avrtools/src
+Terminal --disable-server -T "AVR Tools"  --maximize -e ../init.sh
 zenity --info --text "Press OK to quit/unmount AVR Tools" 
