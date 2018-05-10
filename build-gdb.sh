da9707ab96801ebab488ffca8fc9e1603ec1c32d
Urja Rannikko <urjaman@gmail.com>
2018-05-10 07:58:38 +0000

    fixup gdb build on arch
    TBD if this works on dchrt, fixup that later

--- build-gdb.sh
+++ build-gdb.sh
@@ -2,7 +2,7 @@
 . ./build-cfg
 #gdb
 mkdir gdb; cd gdb
-../../src/gdb-*/configure --prefix=$PREFIX --target=avr --disable-nls --enable-languages=c,c++
+../../src/gdb-*/configure --prefix=$PREFIX --target=avr --disable-nls --enable-languages=c,c++ --with-guile=guile-2.0
 make $MJ
 make install
 
