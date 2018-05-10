cd221004ff7a8bf20c8dfc28eb4b12868b4148df
Urja Rannikko <urjaman@gmail.com>
2018-05-10 08:00:10 +0000

    fix cleanup.sh, strip the AVR libraries and other things learned from arch PKGBUILD

--- cleanup.sh
+++ cleanup.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-. build-cfg
+. ./build-cfg
+set -x
+set +e
 cd $PREFIX/bin
-rm gitk
+rm -f gitk
 strip *
 cd ..
 rm -rf include
 cd lib
-strip --strip-debug *.so
 rm libavr-sim.a
+rm libavrdude.*
+rm libcc1.*
 cd ../libexec
 strip git-core/*
 cd gcc/avr/*/
 strip cc1 cc1plus collect2 lto-wrapper lto1
 cd ../../../../share
 rm -rf info man locale
-
+find $PREFIX/lib -type f -name "*.a" -exec $PREFIX/bin/avr-strip --strip-debug '{}' \;
